b560146c8e21d5a61a7aeef63e8c045f1f75bc1e
tawiah <tawiahin4k@gmail.com>
2024-12-24 08:12:10 +0000

    Add image imports and exports in index.js

---
+++ src/index.js
@@ -0,0 +1,60 @@
+/** @format */
+
+import AboutUsPhoto from "../images/about us photo.jpg";
+import AboutBg from "../images/about-bg.webp";
+import Avatar from "../images/avatar.jpg";
+import Collaborate from "../images/collaborate.gif";
+import Contact from "../images/contact.webp";
+import Gallery from "../images/gallery.webp";
+import Giphy from "../images/giphy.webp";
+import Hero from "../images/hero image.png";
+import Service from "../images/service.gif";
+import TesPic1 from "../images/tes-pic1.jpg";
+import Tes2 from "../images/tes2.jpg";
+import Tes3 from "../images/tes3.jpg";
+import Tes4 from "../images/tes4.jpg";
+import Tes5 from "../images/tes5.jpg";
+import Tes6 from "../images/tes6.jpg";
+
+import Graduation1 from "../images/gallery/graduation1.jpg";
+import Graduation2 from "../images/gallery/graduation2.jpg";
+import Graduation3 from "../images/gallery/graduation3.jpg";
+import Nature from "../images/gallery/nature.jpg";
+import Nature2 from "../images/gallery/nature2.jpg";
+import Nature3 from "../images/gallery/nature3.jpg";
+import Nature4 from "../images/gallery/nature4.jpg";
+import Nature5 from "../images/gallery/nature5.jpg";
+import Wedding1 from "../images/gallery/wedding1.webp";
+import Wedding2 from "../images/gallery/wedding2.jpg";
+import Wedding3 from "../images/gallery/wedding3.jpg";
+import Wedding4 from "../images/gallery/wedding4.webp";
+
+export {
+  AboutUsPhoto,
+  AboutBg,
+  Avatar,
+  Collaborate,
+  Contact,
+  Gallery,
+  Giphy,
+  Hero,
+  Service,
+  TesPic1,
+  Tes2,
+  Tes3,
+  Tes4,
+  Tes5,
+  Tes6,
+  Graduation1,
+  Graduation2,
+  Graduation3,
+  Nature,
+  Nature2,
+  Nature3,
+  Nature4,
+  Nature5,
+  Wedding1,
+  Wedding2,
+  Wedding3,
+  Wedding4,
+};
