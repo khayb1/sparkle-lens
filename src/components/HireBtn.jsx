c8843b070d99c02b28bf63278ada4fee94dc9bc5
Kelvin Afrane <shelvinbrown@gmail.com>
2024-12-31 11:51:15 +0000

    final update

--- src/components/HireBtn.jsx
+++ src/components/HireBtn.jsx
@@ -1,14 +1,15 @@
 /** @format */
 
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const HireBtn = ({ children }) => {
   return (
-    <a
-      href="gallery"
+    <Link
+      to="/gallery"
       className="relative z-10 w-fit inline-block mx-auto px-3 py-3 bg-blue-600 hover:bg-blue-400 hover:text-black text-white font-semibold rounded-md transition"
     >
       {children}
-    </a>
+    </Link>
   );
 };
